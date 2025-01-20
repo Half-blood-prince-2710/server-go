88ab5aabe58b9f6de7dca20b80292be16b045c5c
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-20 20:29:22 +0530

    created blog.go and basic boilerplate for its handlers

---
+++ cmd/api/blog.go
@@ -0,0 +1,20 @@
+package main
+
+import "net/http"
+
+func (app *application)  getAllBlogsHandler(w http.ResponseWriter, r *http.Request){
+	
+}
+
+func (app *application)  createBloghandler(w http.ResponseWriter, r *http.Request){
+	
+}
+
+func (app *application)  updateBlogHandler(w http.ResponseWriter, r *http.Request){
+	
+}
+
+func (app *application)  deleteBlogHanlder(w http.ResponseWriter, r *http.Request){
+	
+}
+
