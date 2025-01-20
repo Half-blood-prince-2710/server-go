909b8130c81959393ae289c3ee95c292799604dd
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-20 20:57:28 +0530

    adding errorlog in server struct and env in config struct

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,16 +10,18 @@ import (
 
 type config struct {
 	Addr string
+	env  string
 }
 
 type application struct {
 	cfg  config 
-	logger slog.Logger
+	logger *slog.Logger
 }
 
 func main() {
 	var cfg config
 	flag.StringVar(&cfg.Addr,"Addr",":5000","Server Port Address")
+	flag.StringVar(&cfg.env,"env","development","service Environment: development|staging|production Default: development")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout,nil))
@@ -27,7 +29,7 @@ func main() {
 	
 	app := &application{
 		cfg : cfg,
-		logger: *logger,
+		logger: logger,
 	}
 
 	srv := &http.Server{
@@ -36,11 +38,13 @@ func main() {
 		WriteTimeout: 30* time.Second,
 		ReadTimeout: 30* time.Second,
 		IdleTimeout: time.Minute,
+		ErrorLog: slog.NewLogLogger(logger.Handler(),slog.LevelError),
 	}
 
 	logger.Info("server is runing at ","Addr",cfg.Addr)
 	err := srv.ListenAndServe()
 	if err!=nil {
 		logger.Error("Error running server")
+		os.Exit(1)
 	}
 }
